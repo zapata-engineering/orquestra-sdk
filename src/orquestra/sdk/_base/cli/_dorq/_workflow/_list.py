86ed2174b380c801fe098347e8aa0be136f5eff0
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-01-16 11:48:48 +0000

    feat: add list glue code
    [ORQSDK-671]

---
+++ src/orquestra/sdk/_base/cli/_dorq/_workflow/_list.py
@@ -0,0 +1,106 @@
+################################################################################
+# © Copyright 2022 Zapata Computing Inc.
+################################################################################
+"""
+Code for 'orq workflow list'.
+"""
+import typing as t
+
+from orquestra.sdk.schema.configs import ConfigName
+from orquestra.sdk.schema.workflow_run import State, WorkflowRun, WorkflowRunId
+
+from .. import _arg_resolvers, _repos
+from .._ui import _presenters
+
+
+class Action:
+    """
+    Encapsulates app-related logic for handling ``orq workflow view``.
+    It's the glue code that connects resolving missing arguments, reading data, and
+    presenting the results back to the user.
+
+    The module is considered part of the name, so this class should be read as
+    ``_dorq._workflow._list.Action``.
+    """
+
+    def __init__(
+        self,
+        presenter=_presenters.WrappedCorqOutputPresenter(),
+        wf_run_repo=_repos.WorkflowRunRepo(),
+        config_resolver: t.Optional[_arg_resolvers.ConfigResolver] = None,
+        wf_run_filter_resolver: t.Optional[_arg_resolvers.WFRunFilterResolver] = None,
+    ):
+        # data sources
+        self._wf_run_repo = wf_run_repo
+
+        # arg resolvers
+        self._config_resolver = config_resolver or _arg_resolvers.ConfigResolver(
+            wf_run_repo=wf_run_repo
+        )
+        self._wf_run_filter_resolver = (
+            wf_run_filter_resolver or _arg_resolvers.WFRunFilterResolver()
+        )
+
+        # text IO
+        self._presenter = presenter
+
+    def on_cmd_call(
+        self,
+        config: t.Optional[t.Iterable[str]],
+        limit: t.Optional[int],
+        max_age: t.Optional[str],
+        state: t.Optional[t.Iterable[str]],
+        show_all: bool = False,
+        interactive: bool = False,
+    ):
+        # This should be handled by the mutually exclusive constraint in the entry, but
+        # we'll keep this here to be safe.
+        assert not (show_all and interactive)
+
+        if (not (show_all or interactive)) and (
+            None in (limit, max_age, state, show_all)
+        ):
+            # neither the interactive nor all flags are set, and one or more filters
+            # are not set. Warn the user that the unset filters will not be applied.
+            filter_dict = {"limit": limit, "max_age": max_age, "state": state}
+
+            unset_filters = [key for key in filter_dict if filter_dict[key] is None]
+            print(
+                "The following filters have not been set and will not be applied: "
+                f"{unset_filters}."
+                "\nTo set filters interactively use the '--interactive (-i)' flag, "
+                "or '--all (-a)' to silence this warning."
+                "\nAvailable filters are:"
+                "\n  '--limit'  : maximum number of results to display for each config."
+                "\n  '--max_age': upper limit for age of results to display."
+                "\n  '--state'  : only show results with the specified state "
+                "(can be used multiple times)."
+            )
+
+        # Resolve Arguments
+        resolved_configs: t.List[ConfigName] = self._config_resolver.resolve_multiple(
+            config
+        )
+        resolved_limit = self._wf_run_filter_resolver.resolve_limit(
+            limit, interactive=interactive
+        )
+        resolved_max_age = self._wf_run_filter_resolver.resolve_max_age(
+            max_age, interactive=interactive
+        )
+        resolved_state = self._wf_run_filter_resolver.resolve_state(
+            state, interactive=interactive
+        )
+
+        # Get wf runs for each config
+        wf_runs: t.List[WorkflowRun] = []
+
+        for resolved_config in resolved_configs:
+            wf_runs += self._wf_run_repo.list_wf_runs(
+                resolved_config,
+                limit=resolved_limit,
+                max_age=resolved_max_age,
+                state=resolved_state,
+            )
+
+        # Display to the user
+        self._presenter.show_wf_runs_list(wf_runs)
---
+++ tests/cli/dorq/workflow/test_list.py
@@ -0,0 +1,164 @@
+################################################################################
+# © Copyright 2022 Zapata Computing Inc.
+################################################################################
+"""
+Unit tests for 'orq wf list' glue code.
+"""
+
+from unittest.mock import Mock
+
+import pytest
+
+from orquestra.sdk._base.cli._dorq._workflow import _list
+
+
+class TestAction:
+    """
+    Test boundaries::
+        [_list.Action]->[arg resolvers]
+                      ->[repos]
+                      ->[presenter]
+    """
+
+    @staticmethod
+    @pytest.mark.parametrize("interactive", [True, False])
+    def test_data_passing(interactive, capsys):
+        """
+        Verifies how we pass variables between subcomponents.
+        """
+        # Given
+        # CLI inputs
+        config = ["<config sentinel>"]
+        limit = "<limit sentinel>"
+        max_age = "<max_age sentinel>"
+        state = "<state sentinel>"
+
+        # Resolved values
+        resolved_configs = ["<resolved config 1>", "<resolved config 2>"]
+        resolved_limit = "<resolved_limit>"
+        resolved_max_age = "<resolved_max_age>"
+        resolved_state = "<resolved_state>"
+
+        # Mocks
+        presenter = Mock()
+
+        wf_run_repo = Mock()
+        wf_runs = ["<wf run sentinel 1>", "<wf run sentinel 2>"]
+        wf_run_repo.list_wf_runs.return_value = wf_runs
+
+        config_resolver = Mock()
+        config_resolver.resolve_multiple.return_value = resolved_configs
+
+        wf_run_filter_resolver = Mock()
+        wf_run_filter_resolver.resolve_limit.return_value = resolved_limit
+        wf_run_filter_resolver.resolve_max_age.return_value = resolved_max_age
+        wf_run_filter_resolver.resolve_state.return_value = resolved_state
+
+        action = _list.Action(
+            presenter=presenter,
+            wf_run_repo=wf_run_repo,
+            config_resolver=config_resolver,
+            wf_run_filter_resolver=wf_run_filter_resolver,
+        )
+
+        # When
+        action.on_cmd_call(
+            config=config,
+            limit=limit,
+            max_age=max_age,
+            state=state,
+            interactive=interactive,
+        )
+
+        # Then
+        # We should pass input config args to config resolver.
+        config_resolver.resolve_multiple.assert_called_with(config)
+
+        # We should pass input filter args to the filter resolver.
+        wf_run_filter_resolver.resolve_limit.assert_called_with(
+            limit, interactive=interactive
+        )
+        wf_run_filter_resolver.resolve_max_age.assert_called_with(
+            max_age, interactive=interactive
+        )
+        wf_run_filter_resolver.resolve_state.assert_called_with(
+            state, interactive=interactive
+        )
+
+        # We should pass resolved values to run repo.
+        for resolved_config in resolved_configs:
+            wf_run_repo.list_wf_runs.assert_any_call(
+                resolved_config,
+                limit=resolved_limit,
+                max_age=resolved_max_age,
+                state=resolved_state,
+            )
+
+        # We expect printing of the workflow runs returned from the repo.
+        expected_wf_runs_list = wf_runs + wf_runs
+        presenter.show_wf_runs_list.assert_called_with(expected_wf_runs_list)
+
+        # This specifies all of the filters, so we shouldn't get anything flagging up
+        # to the user
+        captured = capsys.readouterr()
+        assert captured.out == ""
+        assert captured.err == ""
+
+    @staticmethod
+    def test_show_warning_if_unset_filters_and_no_flags(capsys):
+        """
+        If the interactive and all flags are both unset and the is one or more unset
+        filter, we should get a warning displayed to the user.
+        """
+        # Given
+        # CLI inputs
+        config = ["<config sentinel>"]
+        # limit = "<limit sentinel>"
+        max_age = "<max_age sentinel>"
+        state = "<state sentinel>"
+
+        # Resolved values
+        resolved_configs = ["<resolved config 1>", "<resolved config 2>"]
+        resolved_limit = "<resolved_limit>"
+        resolved_max_age = "<resolved_max_age>"
+        resolved_state = "<resolved_state>"
+
+        # Mocks
+        presenter = Mock()
+
+        wf_run_repo = Mock()
+        wf_runs = ["<wf run sentinel 1>", "<wf run sentinel 2>"]
+        wf_run_repo.list_wf_runs.return_value = wf_runs
+
+        config_resolver = Mock()
+        config_resolver.resolve_multiple.return_value = resolved_configs
+
+        wf_run_filter_resolver = Mock()
+        wf_run_filter_resolver.resolve_limit.return_value = resolved_limit
+        wf_run_filter_resolver.resolve_max_age.return_value = resolved_max_age
+        wf_run_filter_resolver.resolve_state.return_value = resolved_state
+
+        action = _list.Action(
+            presenter=presenter,
+            wf_run_repo=wf_run_repo,
+            config_resolver=config_resolver,
+            wf_run_filter_resolver=wf_run_filter_resolver,
+        )
+
+        # When
+        action.on_cmd_call(
+            config=config,
+            limit=None,
+            max_age=max_age,
+            state=state,
+        )
+
+        # Then
+        captured = capsys.readouterr()
+        assert (
+            "The following filters have not been set and will not be applied: "
+            "['limit']."
+            "\nTo set filters interactively use the '--interactive (-i)' flag, "
+            "or '--all (-a)' to silence this warning."
+        ) in captured.out
+        assert captured.err == ""
