d4efe706fe7c6bbd2af49b0edc530dfc6036ee4d
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-01-16 11:58:12 +0000

    feat: add list glue code
    [ORQSDK-671]

--- src/orquestra/sdk/_base/cli/_dorq/_repos.py
+++ src/orquestra/sdk/_base/cli/_dorq/_repos.py
@@ -40,9 +40,9 @@ class WorkflowRunRepo:
     def list_wf_runs(
         self,
         config: ConfigName,
-        limit: t.Optional[int],
-        max_age: t.Optional[int],
-        state: t.Optional[t.Union[State, t.List[State]]],
+        limit: t.Optional[int] = None,
+        max_age: t.Optional[int] = None,
+        state: t.Optional[t.Union[State, t.List[State]]] = None,
     ) -> t.Sequence[WorkflowRun]:
         """
         Asks the runtime for all workflow runs.
