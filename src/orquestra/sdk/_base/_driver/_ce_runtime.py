20d702a10411891d34d2aaf79198011f90ac6336
James Clark <james.clark@zapatacomputing.com>
2023-01-26 18:51:15 +0000

    chore: Fix syntax after suggestion

--- src/orquestra/sdk/_base/_driver/_ce_runtime.py
+++ src/orquestra/sdk/_base/_driver/_ce_runtime.py
@@ -190,10 +190,8 @@ class CERuntime(RuntimeInterface):
         assert len(result_ids) == 1, "We're currently expecting a single result."
 
         try:
-            return tuple(
-                wf_result = self._client.get_workflow_run_result(result_ids[0])
-                serde.deserialize(wf_result)
-            )
+            wf_result = self._client.get_workflow_run_result(result_ids[0])
+            return tuple(serde.deserialize(wf_result))
         except _exceptions.InvalidTokenError as e:
             raise exceptions.UnauthorizedError(f"{e}") from e
 
