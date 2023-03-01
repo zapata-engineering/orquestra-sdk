7dc718ca2eba645a0ff878ce4c016dc334ce7b36
James Clark <james.clark@zapatacomputing.com>
2023-03-01 17:03:17 +0000

    add docs to each env variable

--- src/orquestra/sdk/_base/_env.py
+++ src/orquestra/sdk/_base/_env.py
@@ -1,5 +1,34 @@
 CONFIG_PATH_ENV = "ORQ_CONFIG_PATH"
+"""
+Used to configure the location of the `config.json`
+Example:
+    ORQ_CONFIG_PATH=/tmp/config.json
+"""
+
 DB_PATH_ENV = "ORQ_DB_PATH"
+"""
+Used to configure the location of the `workflows.db`
+Example:
+    ORQ_DB_PATH=/tmp/workflows.db
+"""
+
 RAY_TEMP_PATH_ENV = "ORQ_RAY_TEMP_PATH"
+"""
+Used to configure the location of Ray's temp storage
+Example:
+    ORQ_RAY_TEMP_PATH=/tmp/ray/temp
+"""
+
 RAY_STORAGE_PATH_ENV = "ORQ_RAY_STORAGE_PATH"
+"""
+Used to configure the location of Ray's persistent storage
+Example:
+    ORQ_RAY_STORAGE_PATH=/tmp/ray/storage
+"""
+
 RAY_DOWNLOAD_GIT_IMPORTS_ENV = "ORQ_RAY_DOWNLOAD_GIT_IMPORTS"
+"""
+Used to configure if Ray downloads Git imports
+Example:
+    ORQ_RAY_DOWNLOAD_GIT_IMPORTS=1
+"""
