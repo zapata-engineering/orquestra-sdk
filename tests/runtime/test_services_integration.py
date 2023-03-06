zapata-engineering/orquestra-sdk

commit a2255ad3daae7bc09de302edd60ed86ab0791d23

chore: Configure Ray plasma storage (#66)
# The problem

Ray's plasma directory needs to be configured in Portal

# This PR's solution

- Configures Ray's plasma directory
- Allows Portal to override this location

# Checklist

_Check that this PR satisfies the following items:_

- [x] Tests have been added for new features/changed behavior (if no new
features have been added, check the box).
- [x] The [changelog file](CHANGELOG.md) has been updated with a
user-readable description of the changes (if the change isn't visible to
the user in any way, check the box).
- [x] The PR's title is prefixed with
`<feat/fix/chore/internal/docs>[!]:`

src/orquestra/sdk/_base/_services.py
@@ -11,7 +11,7 @@ import subprocess
 from pathlib import Path
 from typing import Protocol
 
-from ._env import RAY_STORAGE_PATH_ENV, RAY_TEMP_PATH_ENV
+from ._env import RAY_PLASMA_PATH_ENV, RAY_STORAGE_PATH_ENV, RAY_TEMP_PATH_ENV
 
 ORQUESTRA_BASE_PATH = Path.home() / ".orquestra"
 
@@ -55,6 +55,16 @@ def ray_storage_path():
         return ORQUESTRA_BASE_PATH / "ray_storage"
 
 
+def ray_plasma_path():
+    """
+    See ``ray_temp_path``
+    """
+    try:
+        return Path(os.environ[RAY_PLASMA_PATH_ENV])
+    except KeyError:
+        return ORQUESTRA_BASE_PATH / "ray_plasma"
+
+
 # Timeout for inter-process commands (seconds).
 IPC_TIMEOUT = 20
 
@@ -87,6 +97,7 @@ class RayManager:
                 "--head",
                 f"--temp-dir={ray_temp_path()}",
                 f"--storage={ray_storage_path()}",
+                f"--plasma-directory={ray_plasma_path()}",
             ],
             check=False,
             timeout=IPC_TIMEOUT,

src/orquestra/sdk/_base/_env.py
@@ -26,6 +26,13 @@ Example:
     ORQ_RAY_STORAGE_PATH=/tmp/ray/storage
 """
 
+RAY_PLASMA_PATH_ENV = "ORQ_RAY_PLASMA_PATH"
+"""
+Used to configure the location of Ray's plasma storage
+Example:
+    ORQ_RAY_PLASMA_PATH=/tmp/ray/plasma
+"""
+
 RAY_DOWNLOAD_GIT_IMPORTS_ENV = "ORQ_RAY_DOWNLOAD_GIT_IMPORTS"
 """
 Used to configure if Ray downloads Git imports

tests/runtime/test_services.py
@@ -349,3 +349,24 @@ class TestRayLocation:
 
             # Then
             assert location == Path.home() / ".orquestra" / "ray_storage"
+
+    class TestRayPlasma:
+        def test_with_env(self, monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
+            # Given
+            monkeypatch.setenv("ORQ_RAY_PLASMA_PATH", str(tmp_path))
+
+            # When
+            location = _services.ray_plasma_path()
+
+            # Then
+            assert location == tmp_path
+
+        def test_without_env(self):
+            # Given
+            # Nothing
+
+            # When
+            location = _services.ray_plasma_path()
+
+            # Then
+            assert location == Path.home() / ".orquestra" / "ray_plasma"

commit 131923da8b5ad1ac464b0a9f27692586803bb4c4

fix: Make Ray dirs before starting Ray (#70)
# The problem

Ray was failing to start after #66 because the directory it set may not
exist

# This PR's solution

Ensures all Ray dirs are made before starting Ray

There's an issue with the test on CI, see ORQSDK-771.

# Checklist

_Check that this PR satisfies the following items:_

- [x] Tests have been added for new features/changed behavior (if no new
features have been added, check the box).
- [x] The [changelog file](CHANGELOG.md) has been updated with a
user-readable description of the changes (if the change isn't visible to
the user in any way, check the box).
- [x] The PR's title is prefixed with
`<feat/fix/chore/internal/docs>[!]:`

src/orquestra/sdk/_base/_services.py
@@ -84,6 +84,13 @@ class RayManager:
             subprocess.CalledProcessError: if calling the `ray` CLI failed. This
                 shouldn't happen in regular conditions.
         """
+        ray_temp = ray_temp_path()
+        ray_storage = ray_storage_path()
+        ray_plasma = ray_plasma_path()
+
+        for path in (ray_temp, ray_storage, ray_plasma):
+            path.mkdir(exist_ok=True, parents=True)
+
         # 'ray start' fails if the cluster can't be started, or another problem
         # occurred. I don't know a good way to differentiate between these
         # scenarios, so the strategy is:
@@ -95,9 +102,9 @@ class RayManager:
                 "ray",
                 "start",
                 "--head",
-                f"--temp-dir={ray_temp_path()}",
-                f"--storage={ray_storage_path()}",
-                f"--plasma-directory={ray_plasma_path()}",
+                f"--temp-dir={ray_temp}",
+                f"--storage={ray_storage}",
+                f"--plasma-directory={ray_plasma}",
             ],
             check=False,
             timeout=IPC_TIMEOUT,

tests/runtime/test_services_integration.py
@@ -1,0 +1,22 @@
+################################################################################
+# © Copyright 2022-2023 Zapata Computing Inc.
+################################################################################
+import pytest
+
+from orquestra.sdk._base._services import RayManager
+from orquestra.sdk._base._testing._connections import ray_suitable_temp_dir
+
+
+@pytest.mark.slow
+@pytest.mark.skip("PATH issues on CI: ORQSDK-771")
+def test_ray_roundtrip(monkeypatch: pytest.MonkeyPatch):
+    with ray_suitable_temp_dir() as tmp_path:
+        orq_dir = tmp_path / ".orquestra"
+        monkeypatch.setenv("ORQ_RAY_TEMP_PATH", str(orq_dir / "ray"))
+        monkeypatch.setenv("ORQ_RAY_STORAGE_PATH", str(orq_dir / "ray_storage"))
+        monkeypatch.setenv("ORQ_RAY_PLASMA_PATH", str(orq_dir / "ray_plasma"))
+        ray = RayManager()
+        ray.up()
+        assert ray.is_running()
+        ray.down()
+        assert not ray.is_running()