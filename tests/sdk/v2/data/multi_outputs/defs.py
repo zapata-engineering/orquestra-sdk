9fd0ce0f8076d0761823e598e3c380065200fac5
Alexander Juda <alexander.juda@zapatacomputing.com>
2023-03-29 16:37:08 +0200

    Simplify example

--- tests/sdk/v2/data/multi_outputs/defs.py
+++ tests/sdk/v2/data/multi_outputs/defs.py
@@ -1,7 +1,7 @@
 ################################################################################
 # © Copyright 2023 Zapata Computing Inc.
 ################################################################################
-
+import typing as t
 from orquestra import sdk
 
 
@@ -21,13 +21,37 @@ def wf():
     return a(a(None)), b(b()), b(w, x, y, z)
 
 
+inline_task = sdk.task(source_import=sdk.InlineImport())
+
+
+@inline_task
+def multi_outputs():
+    return 1, 2
+
+
+@inline_task
+def sum_tuple(ints_tuple: t.Tuple[int, ...]) -> int:
+    return sum(ints_tuple)
+
+
+@sdk.workflow
+def wf_simplified():
+    packed = multi_outputs()
+    return sum_tuple(packed)
+
+
 def main():
-    r = wf().run("in_process")
+    wf_def = wf_simplified()
+    r = wf_def.run("in_process")
     r.wait_until_finished()
     tasks = list(r.get_tasks())
-    task = next(t for t in tasks if t.task_invocation_id == "invocation-2-task-b")
+    task = next(
+        t for t in tasks if t.task_invocation_id == "invocation-0-task-sum-tuple"
+    )
     print(task)
     breakpoint()
+
+    wf_def.graph.render("wf")
     print(task.get_inputs())
 
 
