bdd5d58c27699b30ee6b535610ca2fab391e9ea3
James Clark <james.clark@zapatacomputing.com>
2023-04-19 11:41:49 +0100

    Add docstring for retry

--- src/orquestra/sdk/_base/_retry.py
+++ src/orquestra/sdk/_base/_retry.py
@@ -11,6 +11,20 @@ def retry(
     allowed_exceptions: Tuple[Type[Exception]],
     delay: Optional[float] = None,
 ):
+    """
+    A decorator useful for when a function should be retried.
+
+    Usage:
+        @retry(attempts=2, allowed_exceptions=(RuntimeError, ConnectionError))
+        def my_function_that_can_fail():
+            ...
+
+    Args:
+        attempts: the maximum number of times to try the function before raising
+        allowed_exceptions: the exceptions that are allowed to be retried.
+            All other exceptions will not cause the function to be retried.
+        delay: if set, there will be a short pause (sleep) before retrying
+    """
     def _inner(fn):
         def _retried(*args, **kwargs):
             for i in range(attempts):
