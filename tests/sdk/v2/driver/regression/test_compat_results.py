637013bf7ec2225f74c74ae29590deb8d86f1527
James Clark <james.clark@zapatacomputing.com>
2023-04-21 17:32:06 +0100

    update tests

--- tests/sdk/v2/driver/regression/test_compat_results.py
+++ tests/sdk/v2/driver/regression/test_compat_results.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from unittest.mock import Mock
 
 import pydantic
 import pytest
@@ -10,55 +11,62 @@ from orquestra.sdk.schema.responses import JSONResult, PickleResult, WorkflowRes
 DATA_PATH = Path(__file__).parent / "data"
 
 
-class AnyObject:
-    def __eq__(self, _):
-        return True
+class MockedResponse:
+    def __init__(self, json_dict, status_code=200):
+        self._json_dict = json_dict
+        self._status_code = status_code
+
+    def json(self):
+        return self._json_dict
+
+    @property
+    def status_code(self):
+        return self._status_code
+
+    @property
+    def ok(self):
+        return 200 <= self._status_code < 300
 
 
 class TestWorkflowResults:
     @pytest.fixture
-    def mock_results(self, mocked_client):
-        def _inner(scenario_name, sdk_version):
-            ids_json = (
-                DATA_PATH / f"{scenario_name}_ids_{sdk_version}.json"
+    def mock_results(self, monkeypatch):
+        def _inner(sdk_version, scenario=None):
+            extra = "{}".format(
+                "" if scenario is None else f"_{scenario}"
+            )
+            status_json = (
+                DATA_PATH / f"{sdk_version}{extra}_status.json"
             ).read_text()
-            result_path = DATA_PATH / f"{scenario_name}_{sdk_version}.json"
-            mocked_client.get_workflow_run_results.return_value = json.loads(ids_json)[
-                "data"
+            ids_json = (
+                DATA_PATH / f"{sdk_version}{extra}_ids.json"
+            ).read_text()
+            result_json = (DATA_PATH / f"{sdk_version}{extra}.json").read_text()
+            client_get = Mock()
+            client_get.side_effect = [
+                MockedResponse(json.loads(ids_json)),
+                MockedResponse(json.loads(result_json))
             ]
-            mocked_client.get_workflow_run_result.side_effect = [
-                pydantic.parse_file_as(WorkflowResult, result_path)
-            ]
+            monkeypatch.setattr(
+                "orquestra.sdk._base._driver._client.DriverClient._get", client_get
+            )
 
         return _inner
 
     @pytest.mark.parametrize(
-        "sdk_version, expected",
+        "sdk_version, scenario, expected",
         [
-            ("v0.46.0", ([100],)),
-        ],
-    )
-    def test_single_workflow_output(
-        self, mock_results, runtime, workflow_run_id, sdk_version, expected
-    ):
-        mock_results("single_result", sdk_version)
-
-        results = runtime.get_workflow_run_outputs_non_blocking(workflow_run_id)
-
-        assert isinstance(results, tuple)
-        deserialised = tuple(deserialize(r) for r in results)
-        assert deserialised == expected
-
-    @pytest.mark.parametrize(
-        "sdk_version, expected",
-        [
-            ("v0.46.0", ((100, AnyObject(), 100),)),
+            ("v0.46.0", "json", ([100, "json_string"],)),
+            ("v0.46.0", "json-single", ([100],)),
+            ("v0.46.0", "pickle", ((100, "pickled_string"),)),
+            ("v0.46.0", "pickle-single", (("pickled_string",),)),
+            ("v0.47.0", None, (100, 100)),
         ],
     )
     def test_multi_workflow_outputs(
-        self, mock_results, runtime, workflow_run_id, sdk_version, expected
+        self, mock_results, runtime, workflow_run_id, sdk_version, scenario, expected
     ):
-        mock_results("multi_results", sdk_version)
+        mock_results(sdk_version, scenario)
 
         results = runtime.get_workflow_run_outputs_non_blocking(workflow_run_id)
 
