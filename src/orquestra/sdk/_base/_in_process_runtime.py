09793dfd4344ab96c390e93e233a22a19644e9fb
James Clark <70290797+jamesclark-Zapata@users.noreply.github.com>
2023-05-31 12:08:44 +0100

    feat: secret with workspaces in IR and GithubImport (#185)
    # The problem
    
    We missed some places where secrets can be used when adding workspace
    support.
    
    # This PR's solution
    
    * Update `orquestra.sdk.Secret` to take `workspace_id` as an argument.
    * Update `orquestra.sdk.schema.ir.SecretNode` to take `workspace_id` as
    an argument.
    * Update in-process runtime to handle `workspace_id`.
    * Update Ray runtime to handle `workspace_id`.
    * Update graph traversal to handle `workspace_id`.
    * Update in-workflow `orquestra.sdk.secrets.get()` handle
    `workspace_id`.
    * Update `dispatch.py` to handle `workspace_id`.
    
    # Checklist
    
    _Check that this PR satisfies the following items:_
    
    - [x] Tests have been added for new features/changed behavior (if no new
    features have been added, check the box).
    - [x] The [changelog file](CHANGELOG.md) has been updated with a
    user-readable description of the changes (if the change isn't visible to
    the user in any way, check the box).
    - [x] The PR's title is prefixed with
    `<feat/fix/chore/internal/docs>[!]:`
    - [x] The PR is linked to a JIRA ticket (if there's no suitable ticket,
    check the box).

--- src/orquestra/sdk/_base/_in_process_runtime.py
+++ src/orquestra/sdk/_base/_in_process_runtime.py
@@ -151,7 +151,9 @@ class InProcessRuntime(abc.RuntimeInterface):
         }
         for id, secret in workflow_def.secret_nodes.items():
             consts[id] = secrets.get(
-                secret.secret_name, config_name=secret.secret_config
+                secret.secret_name,
+                config_name=secret.secret_config,
+                workspace_id=secret.workspace_id,
             )
         # We'll store artifacts for this run here.
         self._artifact_store[run_id] = {}
