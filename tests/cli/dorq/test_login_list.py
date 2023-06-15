093a89a844ba88eec1b62c27eb4af37c9d0cae27
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-06-15 17:43:31 +0100

    test: add tests for login list action.

---
+++ tests/cli/dorq/test_login_list.py
@@ -0,0 +1,59 @@
+################################################################################
+# © Copyright 2023 Zapata Computing Inc.
+################################################################################
+
+from unittest.mock import Mock, create_autospec
+
+from orquestra.sdk._base.cli._dorq._config._list import Action
+from orquestra.sdk.schema.configs import RuntimeConfiguration
+
+
+class TestAction:
+    @staticmethod
+    def test_data_passing(monkeypatch):
+        # Given
+        exception_presenter = Mock()
+        config_presenter = Mock()
+        config_repo = Mock()
+        prompter = Mock()
+        monkeypatch.setattr(
+            "orquestra.sdk._base._jwt.check_jwt_without_signature_verification",
+            Mock(return_value=True),
+        )
+
+        config_repo.list_remote_config_names = Mock(
+            return_value=["<config name sentinel 1>", "<config name sentinel 2>"]
+        )
+        configs = [
+            create_autospec(RuntimeConfiguration),
+            create_autospec(RuntimeConfiguration),
+        ]
+        configs[0].runtime_options = {"token": "<token sentinel 1>"}
+        configs[0].config_name = "<config name sentinel 3>"
+        configs[1].runtime_options = {"token": "<token sentinel 2>"}
+        configs[1].config_name = "<config name sentinel 4>"
+        config_repo.read_config = Mock(side_effect=configs)
+
+        action = Action(
+            exception_presenter=exception_presenter,
+            config_presenter=config_presenter,
+            config_repo=config_repo,
+            prompter=prompter,
+        )
+
+        # When
+        action._on_cmd_call_with_exceptions()
+
+        # Then
+        exception_presenter.assert_not_called()
+        config_presenter.print_configs_list.assert_called_once_with(
+            configs,
+            {"<config name sentinel 3>": False, "<config name sentinel 4>": False},
+            message="Stored Logins:",
+        )
+        config_repo.list_remote_config_names.assert_called_once_with()
+        assert [call.args for call in config_repo.read_config.call_args_list] == [
+            ("<config name sentinel 1>",),
+            ("<config name sentinel 2>",),
+        ]
+        prompter.assert_not_called()
