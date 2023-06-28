1266dbf9c5b01c8ac2ce43303a1a6651147d211d
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2023-06-28 14:59:32 +0200

    int: emit task start/end markers (#217)
    # The problem
    
    Looks like we're going to rely on task start/end markers to correlate
    Orquestra tasks with logs to support arbitrary stdout/stderr.
    `RayRuntime` needs to emit them.
    
    # This PR's solution
    
    Adds the `_marker` module to cover the marker emitting and parsing. The
    emitting part is used in our Ray code. The parsing part isn't used in
    this PR, but will be used soon to power the local runtime. This PR
    contains no changes from the user's perspective, yet. The marker format
    is also described in [an
    ADR](https://zapatacomputing.atlassian.net/wiki/spaces/ORQSRUN/pages/723517467/2023-06-27+Format+For+Task+Marker+Logs).
    
    I had a draft implementation of both sides of the equation (emitting
    markers and correlating logs):
    https://github.com/zapatacomputing/orquestra-workflow-sdk/pull/214. I
    decided to split the work into two smaller PRs for easier discussion.
    
    This branch includes two commits: code changes and fixture updates. I
    recommend reviewing them separately to avoid being overwhelmed with
    diffs.
    
    # Checklist
    
    _Check that this PR satisfies the following items:_
    
    - [x] Tests have been added for new features/changed behavior (if no new
    features have been added, check the box).
    - [x] The [changelog file](CHANGELOG.md) has been updated with a
    user-readable description of the changes (if the change isn't visible to
    the user in any way, check the box).
    - [x] The PR's title is prefixed with
    `<feat/fix/chore/imp[rovement]/int[ernal]/docs>[!]:`
    - [x] The PR is linked to a JIRA ticket (if there's no suitable ticket,
    check the box).

---
+++ tests/sdk/v2/logs/test_markers.py
@@ -0,0 +1,163 @@
+################################################################################
+# © Copyright 2023 Zapata Computing Inc.
+################################################################################
+"""
+Unit tests for ``orquestra.sdk._base._logs._markers``.
+"""
+
+import json
+import sys
+
+import pytest
+
+from orquestra.sdk._base import _dates
+from orquestra.sdk._base._logs import _markers
+
+INSTANT = _dates.from_isoformat("2005-04-25T20:37:00+00:00")
+
+
+class TestParseLine:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "marker",
+        [
+            _markers.TaskStartMarker(
+                wf_run_id="wf1", task_inv_id="inv1", timestamp=INSTANT
+            ),
+            _markers.TaskEndMarker(
+                wf_run_id="wf1", task_inv_id="inv1", timestamp=INSTANT
+            ),
+            _markers.TaskEndMarker(
+                wf_run_id="wf1", task_inv_id=None, timestamp=INSTANT
+            ),
+            _markers.TaskEndMarker(
+                wf_run_id=None, task_inv_id="inv1", timestamp=INSTANT
+            ),
+            _markers.TaskEndMarker(wf_run_id=None, task_inv_id=None, timestamp=INSTANT),
+        ],
+    )
+    def test_valid(marker: _markers.Marker):
+        # Given
+        line = marker.line
+
+        # When
+        parsed = _markers.parse_line(line)
+
+        # Then
+        assert parsed == marker
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "line",
+        [
+            pytest.param("", id="empty-line"),
+            pytest.param("ORQ-MARKER:", id="only-prefix"),
+            pytest.param("ORQ-MARKER:{}", id="empty-event"),
+            pytest.param(
+                f'ORQ-MARKER:{json.dumps({"event": "task_start"})}',
+                id="malformed-start-event",
+            ),
+            pytest.param(
+                f'ORQ-MARKER:{json.dumps({"event": "other"})}',
+                id="unsupported-event-type",
+            ),
+            pytest.param(":job_id:01000000", id="ray-job-marker"),
+            pytest.param("actor_name:WorkflowManagementActor", id="ray-actor-marker"),
+            pytest.param(
+                "023-06-12 14:46:54,274	INFO workflow_executor.py:86 -- Workflow job [id=wf.wf_using_python_imports.8a4d9e7] started.",  # noqa: E501
+                id="ray-workflow-marker",
+            ),
+            pytest.param(
+                "023-06-12 14:46:55,385	INFO workflow_executor.py:284 -- Task status [SUCCESSFUL]	[wf.wf_using_python_imports.8a4d9e7@invocation-1-task-add-with-log]",  # noqa: E501
+                id="ray-task-marker",
+            ),
+        ],
+    )
+    def test_invalid(line):
+        # When
+        parsed = _markers.parse_line(line)
+
+        # Then
+        assert parsed is None
+
+
+class TestPrintedTaskMarkers:
+    @staticmethod
+    def test_happy_flow(capsys):
+        # Given
+        wf_run_id = "wf1"
+        task_inv_id = "inv1"
+        message = "hello!"
+
+        # When
+        with _markers.printed_task_markers(
+            wf_run_id=wf_run_id,
+            task_inv_id=task_inv_id,
+        ):
+            print(message)
+            print(message, file=sys.stderr)
+
+        # Then
+        captured = capsys.readouterr()
+        for stream in [captured.out, captured.err]:
+            lines = stream.splitlines()
+            assert len(lines) == 3
+
+            assert isinstance(_markers.parse_line(lines[0]), _markers.TaskStartMarker)
+            assert lines[1] == message
+            assert isinstance(_markers.parse_line(lines[2]), _markers.TaskEndMarker)
+
+    @staticmethod
+    # TODO: un-skip with https://zapatacomputing.atlassian.net/browse/ORQSDK-872
+    @pytest.mark.skip(reason="Ray workflow builder captures exceptions")
+    def test_exception(capsys):
+        # Given
+        wf_run_id = "wf1"
+        task_inv_id = "inv1"
+        message = "uh oh!"
+
+        # When
+        with pytest.raises(ValueError):
+            with _markers.printed_task_markers(
+                wf_run_id=wf_run_id,
+                task_inv_id=task_inv_id,
+            ):
+                raise ValueError(message)
+
+        # Then
+        captured = capsys.readouterr()
+        out_lines = captured.out.splitlines()
+        assert len(out_lines) == 2
+
+        assert isinstance(_markers.parse_line(out_lines[0]), _markers.TaskStartMarker)
+        assert isinstance(_markers.parse_line(out_lines[1]), _markers.TaskEndMarker)
+
+        err_lines = captured.err.splitlines()
+        assert len(err_lines) > 2
+
+        assert isinstance(_markers.parse_line(out_lines[0]), _markers.TaskStartMarker)
+        assert isinstance(_markers.parse_line(out_lines[-1]), _markers.TaskEndMarker)
+
+    @staticmethod
+    def test_no_ids(capsys):
+        # TODO: remove this test case
+        # https://zapatacomputing.atlassian.net/browse/ORQSDK-530
+        # Given
+        wf_run_id = None
+        task_inv_id = None
+        message = "hello!"
+
+        # When
+        with _markers.printed_task_markers(
+            wf_run_id=wf_run_id,
+            task_inv_id=task_inv_id,
+        ):
+            print(message)
+            print(message, file=sys.stderr)
+
+        # Then
+        captured = capsys.readouterr()
+        for stream in [captured.out, captured.err]:
+            lines = stream.splitlines()
+            assert len(lines) == 1
+            assert lines[0] == message
