2177d466fc9edc602dc9ce4a4d3d01810661faf0
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-06-30 12:51:39 +0100

    test: add tests for creating dir.
    [ORQSDK-880]

--- tests/sdk/v2/mlflow/test_connection_utils.py
+++ tests/sdk/v2/mlflow/test_connection_utils.py
@@ -2,22 +2,66 @@
 # © Copyright 2023 Zapata Computing Inc.
 ################################################################################
 
-import os
-from pathlib import Path
-from unittest import mock
+import pathlib
 
 from orquestra import sdk
 
 
 class TestGetTempArtifactsDir:
-    @staticmethod
-    def test_with_studio_or_ce(tmp_path):
-        with mock.patch.dict(os.environ, {"ORQ_MLFLOW_ARTIFACTS_DIR": str(tmp_path)}):
+    class TestWithRemote:
+        """
+        'Remote' here covers studio and CE as these are treated identically in the code.
+        """
+
+        @staticmethod
+        def test_happy_path(tmp_path: pathlib.Path, monkeypatch):
+            # Given
+            monkeypatch.setenv("ORQ_MLFLOW_ARTIFACTS_DIR", str(tmp_path))
+
+            # When
             path = sdk.mlflow.get_temp_artifacts_dir()
-        assert path == tmp_path
 
-    @staticmethod
-    def test_with_local():
-        path = sdk.mlflow.get_temp_artifacts_dir()
+            # Then
+            assert path == tmp_path
 
-        assert path == Path.home() / ".orquestra" / "mlflow" / "artifacts"
+        @staticmethod
+        def test_creates_dir(tmp_path: pathlib.Path, monkeypatch):
+            # Given
+            dir = tmp_path / "dir_that_does_not_exist"
+            assert not dir.exists()
+            monkeypatch.setenv("ORQ_MLFLOW_ARTIFACTS_DIR", str(dir))
+
+            # When
+            _ = sdk.mlflow.get_temp_artifacts_dir()
+
+            # Then
+            assert dir.exists()
+
+    class TestWithLocal:
+        @staticmethod
+        def test_happy_path(tmp_path: pathlib.Path, monkeypatch):
+            # Given
+            monkeypatch.setattr(
+                sdk.mlflow._connection_utils, "DEFAULT_TEMP_ARTIFACTS_DIR", tmp_path
+            )
+
+            # When
+            path = sdk.mlflow.get_temp_artifacts_dir()
+
+            # Then
+            assert path == tmp_path
+
+        @staticmethod
+        def test_creates_dir(tmp_path: pathlib.Path, monkeypatch):
+            # Given
+            dir = tmp_path / "dir_that_does_not_exist"
+            assert not dir.exists()
+            monkeypatch.setattr(
+                sdk.mlflow._connection_utils, "DEFAULT_TEMP_ARTIFACTS_DIR", dir
+            )
+
+            # When
+            _ = sdk.mlflow.get_temp_artifacts_dir()
+
+            # Then
+            assert dir.exists()
