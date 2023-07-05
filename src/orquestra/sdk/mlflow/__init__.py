64be69b44b3f825077876709e126ef06a7a32de7
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-07-05 10:37:17 +0100

    docs: docstring for orquestra.mlflow
    [ORQSDK-880]

--- src/orquestra/sdk/mlflow/__init__.py
+++ src/orquestra/sdk/mlflow/__init__.py
@@ -2,6 +2,8 @@
 # © Copyright 2023 Zapata Computing Inc.
 ################################################################################
 
+"""A set of Orquestra utilities relating to interacting with the MLFlow library."""
+
 from ._connection_utils import get_temp_artifacts_dir
 
 __all__ = ["get_temp_artifacts_dir"]
