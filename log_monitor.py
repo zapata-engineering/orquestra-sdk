45989830366a1c09b8f07ff3c912e1d71b820fce
Alexander Juda <alexander.juda@zapatacomputing.com>
2023-07-11 13:03:51 +0200

    Add watchdog poc

--- log_monitor.py
+++ log_monitor.py
@@ -1,11 +1,27 @@
+import logging
+import time
 import watchfiles
+import watchdog
+import watchdog.events
+import watchdog.observers
 from pprint import pprint
 
 
-def main():
+def main1():
     for changes in watchfiles.watch("."):
         pprint(list(changes))
 
 
+def main2():
+    logging.basicConfig(level=logging.INFO)
+    handler = watchdog.events.LoggingEventHandler()
+    observer = watchdog.observers.Observer()
+    observer.schedule(handler, ".", recursive=True)
+    observer.start()
+
+    while True:
+        time.sleep(1)
+
+
 if __name__ == "__main__":
-    main()
+    main2()
