9a098b4e3bd0b4ecd4c98260caa728d70f5a91b8
James Clark <james.clark@zapatacomputing.com>
2023-07-26 18:24:22 +0100

    update copyright date

--- src/orquestra/sdk/_base/_driver/_exceptions.py
+++ src/orquestra/sdk/_base/_driver/_exceptions.py
@@ -1,5 +1,5 @@
 ################################################################################
-# © Copyright 2022 Zapata Computing Inc.
+# © Copyright 2022 - 2023 Zapata Computing Inc.
 ################################################################################
 """
 Exception types related to the Workflow Driver API.
