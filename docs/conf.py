dea3a0677fc5628c2d99ac7fc390d41d9cf49643
Alexander Juda <alexander.juda@zapatacomputing.com>
2023-08-29 13:22:12 +0200

    Remove images

--- docs/conf.py
+++ docs/conf.py
@@ -67,14 +67,4 @@ exclude_patterns = [
 
 html_theme = "furo"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-
-html_logo = "_static/orquestra.png"
-html_title = "Orquestra Documentation"
-html_favicon = "_static/favicon.ico"
-
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
