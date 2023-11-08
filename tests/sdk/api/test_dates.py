d051b897c3b0722aabfc31eb679744ca58887ec8
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-08 11:18:42 +0000

    test: add tests for initialisation from iso string.
    [ORQSDK-945]

--- tests/sdk/api/test_dates.py
+++ tests/sdk/api/test_dates.py
@@ -5,8 +5,9 @@
 Tests for orquestra.sdk._base._dates.py
 """
 
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
+import pytest
 from freezegun import freeze_time
 
 from orquestra.sdk import SDKInstant
@@ -21,3 +22,24 @@ class TestInitiation:
         assert isinstance(my_instant, SDKInstant)
         assert my_instant == datetime.now(timezone.utc)
         assert str(my_instant) == "1312-01-01 04:00:00+00:00"
+
+    @staticmethod
+    @freeze_time("1312-01-01", tz_offset=4)
+    @pytest.mark.parametrize(
+        "iso_string, expected_datetime",
+        [
+            ("2007-04-05T14:30Z", datetime(2007, 4, 5, 14, 30, tzinfo=timezone.utc)),
+            (
+                "1312-01-01T11:00+00:00",
+                datetime(1312, 1, 1, 11, 0, tzinfo=timezone.utc),
+            ),
+            (
+                "1312-01-01T11:00+04:00",
+                datetime(1312, 1, 1, 11, 0, tzinfo=timezone(timedelta(seconds=14400))),
+            ),
+        ],
+    )
+    def test_from_iso_formatted_string(iso_string, expected_datetime):
+        my_instant = SDKInstant(iso_string)
+
+        assert my_instant == expected_datetime, my_instant
