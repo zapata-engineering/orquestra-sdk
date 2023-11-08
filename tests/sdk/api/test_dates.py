a104b4764112f61946ed50b80aaed09f4ff4eb90
Benjamin Mummery <benjamin.mummery13@gmail.com>
2023-11-08 11:50:01 +0000

    refactor: subcategorise tests.
    [ORQSDK-945]

--- tests/sdk/api/test_dates.py
+++ tests/sdk/api/test_dates.py
@@ -13,69 +13,74 @@ from freezegun import freeze_time
 from orquestra.sdk import SDKInstant
 
 
-class TestInitiation:
-    @staticmethod
-    @freeze_time("1312-01-01", tz_offset=4)
-    def test_current_instant():
-        my_instant = SDKInstant()
-        now = datetime.now(timezone.utc)
+class TestSDKInstant:
+    class TestInitiation:
+        @staticmethod
+        @freeze_time("1312-01-01", tz_offset=4)
+        def test_current_instant():
+            my_instant = SDKInstant()
+            now = datetime.now(timezone.utc)
 
-        assert isinstance(my_instant, SDKInstant)
-        assert my_instant == now, (
-            f"Expected {now}, got {my_instant} "
-            f"(difference of {now - my_instant._datetime_object})"
+            assert isinstance(my_instant, SDKInstant)
+            assert my_instant == now, (
+                f"Expected {now}, got {my_instant} "
+                f"(difference of {now - my_instant._datetime_object})"
+            )
+            assert str(my_instant) == "1312-01-01 04:00:00+00:00"
+
+        @staticmethod
+        @pytest.mark.parametrize(
+            "iso_string, expected_datetime",
+            [
+                (
+                    "2007-04-05T14:30Z",
+                    datetime(2007, 4, 5, 14, 30, tzinfo=timezone.utc),
+                ),
+                (
+                    "1312-01-01T11:00+00:00",
+                    datetime(1312, 1, 1, 11, 0, tzinfo=timezone.utc),
+                ),
+                (
+                    "1312-01-01T11:00+04:00",
+                    datetime(
+                        1312, 1, 1, 11, 0, tzinfo=timezone(timedelta(seconds=14400))
+                    ),
+                ),
+            ],
         )
-        assert str(my_instant) == "1312-01-01 04:00:00+00:00"
+        def test_from_iso_formatted_string(
+            iso_string: str, expected_datetime: datetime
+        ):
+            my_instant = SDKInstant(iso_string)
 
-    @staticmethod
-    @freeze_time("9999-01-01", tz_offset=4)
-    @pytest.mark.parametrize(
-        "iso_string, expected_datetime",
-        [
-            ("2007-04-05T14:30Z", datetime(2007, 4, 5, 14, 30, tzinfo=timezone.utc)),
-            (
-                "1312-01-01T11:00+00:00",
-                datetime(1312, 1, 1, 11, 0, tzinfo=timezone.utc),
-            ),
-            (
-                "1312-01-01T11:00+04:00",
-                datetime(1312, 1, 1, 11, 0, tzinfo=timezone(timedelta(seconds=14400))),
-            ),
-        ],
-    )
-    def test_from_iso_formatted_string(iso_string: str, expected_datetime: datetime):
-        my_instant = SDKInstant(iso_string)
+            assert my_instant == expected_datetime, (
+                f"Expected {expected_datetime}, got {my_instant} "
+                f"(difference of {expected_datetime - my_instant._datetime_object})"
+            )
 
-        assert my_instant == expected_datetime, (
-            f"Expected {expected_datetime}, got {my_instant} "
-            f"(difference of {expected_datetime - my_instant._datetime_object})"
+        @staticmethod
+        @pytest.mark.parametrize(
+            "timestamp, expected_datetime",
+            [
+                (-20764486800.0, datetime(1312, 1, 1, 7, 0, tzinfo=timezone.utc)),
+            ],
         )
+        def test_from_unix_timestamp(timestamp: float, expected_datetime: datetime):
+            my_instant = SDKInstant(timestamp)
 
-    @staticmethod
-    @freeze_time("9999-01-01", tz_offset=4)
-    @pytest.mark.parametrize(
-        "timestamp, expected_datetime",
-        [
-            (-20764486800.0, datetime(1312, 1, 1, 7, 0, tzinfo=timezone.utc)),
-        ],
-    )
-    def test_from_unix_timestamp(timestamp: float, expected_datetime: datetime):
-        my_instant = SDKInstant(timestamp)
+            assert my_instant == expected_datetime, (
+                f"Expected {expected_datetime}, got {my_instant} "
+                f"(difference of {expected_datetime - my_instant._datetime_object})"
+            )
 
-        assert my_instant == expected_datetime, (
-            f"Expected {expected_datetime}, got {my_instant} "
-            f"(difference of {expected_datetime - my_instant._datetime_object})"
+        @staticmethod
+        @pytest.mark.parametrize(
+            "datetime", [datetime(1312, 1, 1, 7, 0, tzinfo=timezone.utc)]
         )
+        def test_from_datetime(datetime: datetime):
+            my_instant = SDKInstant(datetime)
 
-    @staticmethod
-    @freeze_time("9999-01-01", tz_offset=4)
-    @pytest.mark.parametrize(
-        "datetime", [datetime(1312, 1, 1, 7, 0, tzinfo=timezone.utc)]
-    )
-    def test_from_datetime(datetime: datetime):
-        my_instant = SDKInstant(datetime)
-
-        assert my_instant == datetime, (
-            f"Expected {datetime}, got {my_instant} "
-            f"(difference of {datetime - my_instant._datetime_object})"
-        )
+            assert my_instant == datetime, (
+                f"Expected {datetime}, got {my_instant} "
+                f"(difference of {datetime - my_instant._datetime_object})"
+            )
