09b2067c5ce9fae0cebb4baf75ce1f7164ec9f65
Alexander Juda <alexander.juda@zapata.ai>
2023-12-18 17:49:39 +0100

    Prep tests with flight client

--- src/orquestra/sdk/dremio/_api.py
+++ src/orquestra/sdk/dremio/_api.py
@@ -3,6 +3,9 @@
 ################################################################################
 from dataclasses import dataclass 
 
+import pandas as pd
+from pyarrow.flight import FlightClient
+
 class DremioClient:
     @dataclass(frozen=True)
     class Config:
@@ -14,3 +17,9 @@ class DremioClient:
     @classmethod
     def from_config(cls, cfg: Config) -> "DremioClient":
         return cls()
+
+    def __init__(self, flight_client: FlightClient):
+        self._flight_client = flight_client
+
+    def read_query(self, query: str) -> pd.DataFrame:
+        pass
--- tests/sdk/dremio/test_dremio_api.py
+++ tests/sdk/dremio/test_dremio_api.py
@@ -1,23 +1,58 @@
-from unittest.mock import sentinel
-import pytest
+from unittest.mock import Mock, create_autospec, sentinel
 
 
 from orquestra.sdk.dremio import DremioClient
+from pyarrow.flight import FlightClient, FlightEndpoint, FlightStreamReader
 
 
 class TestDremioClient:
     @staticmethod
-    def test_basic_init():
+    def test_basic_init(monkeypatch):
         # Given
+        user = "test_user"
+        password = "test_password"
+        host = "grpc+tls://test-host.orquestra.io"
+        port = 2037
         cfg = DremioClient.Config(
-            user=sentinel.user,
-            password=sentinel.password,
-            host=sentinel.host,
-            port=sentinel.port,
+            user=user,
+            password=password,
+            host=host,
+            port=port,
         )
+
+        cert_content = b"test cert contents"
+
+        flight_init_spy = Mock(name="FlightClient.__init__")
+        monkeypatch.setattr(FlightClient, "__init__", flight_init_spy)
 
         # When
         client = DremioClient.from_config(cfg)
 
         # Then
         assert client is not None
+        assert client._flight_client is not None
+        flight_init_spy.assert_called_with("grpc+tls://test-host.orquestra.io:2037", tls_root_certs=cert_content)
+
+    @staticmethod
+    def test_using_flight_client():
+        # Given
+        flight_client = create_autospec(FlightClient, name="flight_client")
+
+        flight_endpoint = create_autospec(FlightEndpoint, "flight_endpoint")
+        flight_client.get_flight_info().endpoints = [flight_endpoint]
+
+        reader = create_autospec(FlightStreamReader)
+        df_sentinel = sentinel.df
+        reader.read_pandas.return_value = df_sentinel
+
+        flight_client.do_get.return_value = reader
+
+        client = DremioClient(flight_client=flight_client)
+
+        query = "shouldn't matter"
+
+        # When
+        df = client.read_query(query)
+
+        # Then
+        assert df == df_sentinel
