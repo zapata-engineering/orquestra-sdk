f28517c18bc21a21f890b2e54679046a0397483c
James Clark <james.clark@zapata.ai>
2024-03-07 11:24:54 -0500

    add tests

---
+++ tests/cli/ui/exception_makers.py
@@ -0,0 +1,51 @@
+################################################################################
+# © Copyright 2024 Zapata Computing Inc.
+################################################################################
+def except_from():
+    try:
+        try:
+            raise KeyError("key")
+        except KeyError as e:
+            raise ValueError("Invalid file") from e
+    except ValueError as e:
+        raise RuntimeError("Unable to do thing") from e
+
+
+def except_from_within_except():
+    try:
+        try:
+            raise KeyError("key")
+        except KeyError as e:
+            raise ValueError("Invalid file")
+    except ValueError as e:
+        raise RuntimeError("Unable to do thing") from e
+
+
+def except_within_except():
+    try:
+        try:
+            raise KeyError("key")
+        except KeyError as e:
+            raise ValueError("Invalid file")
+    except ValueError as e:
+        raise RuntimeError("Unable to do thing")
+
+
+def except_no_message():
+    raise RuntimeError()
+
+
+def except_plain():
+    raise RuntimeError("Unable to do thing")
+
+
+def _a():
+    except_plain()
+
+
+def _b():
+    _a()
+
+
+def except_stack():
+    _b()
