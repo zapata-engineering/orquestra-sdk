ceca7daae270a35047112aa05699f24eaa270edf
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2024-04-11 16:55:27 +0200

    packaging shim

---
+++ src/orquestra/sdk/packaging/__init__.py
@@ -0,0 +1,9 @@
+################################################################################
+# © Copyright 2024 Zapata Computing Inc.
+################################################################################
+# THIS FILE IS A SHIM TO REEXPORT SYMBOLS AS PUBLIC API
+# DO NOT PUT ANY LOGIC INTO THIS FILE.
+
+from orquestra.sdk.shared.packaging import PackagingError, get_installed_version
+
+__all__ = ["get_installed_version", "PackagingError"]
