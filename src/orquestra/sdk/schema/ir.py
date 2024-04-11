fbb245a8b4f845fd3f6b76686a9b43fd80e4d94e
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2024-04-11 16:40:44 +0200

    fix responses shim

--- src/orquestra/sdk/schema/ir.py
+++ src/orquestra/sdk/schema/ir.py
@@ -1,3 +1,6 @@
+################################################################################
+# © Copyright 2024 Zapata Computing Inc.
+################################################################################
 # THIS FILE IS A SHIM TO REEXPORT MLFLOW CLIENT AS A PUBLIC API.
 # DO NOT PUT ANY LOGIC INTO THIS FILE.
 
--- src/orquestra/sdk/schema/responses.py
+++ src/orquestra/sdk/schema/responses.py
@@ -1,1 +1,27 @@
-from orquestra.sdk.shared.schema.responses import *  # NOQA
+################################################################################
+# © Copyright 2024 Zapata Computing Inc.
+################################################################################
+# THIS FILE IS A SHIM TO REEXPORT MLFLOW CLIENT AS A PUBLIC API.
+# DO NOT PUT ANY LOGIC INTO THIS FILE.
+
+from orquestra.sdk.shared.schema.responses import (
+    ComputeEngineWorkflowResult,
+    JSONResult,
+    PickleResult,
+    ResponseFormat,
+    ResponseMetadata,
+    ResponseStatusCode,
+    ServiceResponse,
+    WorkflowResult,
+)
+
+__all__ = [
+    "ResponseFormat",
+    "ResponseStatusCode",
+    "ResponseMetadata",
+    "JSONResult",
+    "PickleResult",
+    "WorkflowResult",
+    "ComputeEngineWorkflowResult",
+    "ServiceResponse",
+]
