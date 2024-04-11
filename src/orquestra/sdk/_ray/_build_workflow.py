b8fc3acc782ce6886b97b783c9ec83dd78a95ab2
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2024-04-11 16:59:39 +0200

    fix _ray shim

--- src/orquestra/sdk/_ray/_build_workflow.py
+++ src/orquestra/sdk/_ray/_build_workflow.py
@@ -1,1 +1,10 @@
-from orquestra.sdk.runtime._ray._build_workflow import TaskResult  # NOQA
+################################################################################
+# © Copyright 2024 Zapata Computing Inc.
+################################################################################
+# THIS FILE IS A SHIM TO REEXPORT SYMBOLS FOR BACKWARDS COMPATIBILITY
+# WITH OLD WORKFLOW RESULTS.
+# DO NOT PUT ANY LOGIC INTO THIS FILE.
+
+from orquestra.sdk.runtime._ray._build_workflow import TaskResult
+
+__all__ = ["TaskResult"]
