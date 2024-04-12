zapatacomputing/orquestra-workflow-sdk

fix: log dumpers

src/orquestra/sdk/_base/cli/_dumpers.py
@@ -1,5 +1,5 @@
 ################################################################################
-# © Copyright 2023 Zapata Computing Inc.
+# © Copyright 2023-2024 Zapata Computing Inc.
 ################################################################################
 
 """Code that stores values on disk as a result of a CLI command."""
@@ -8,7 +8,7 @@ from functools import singledispatchmethod
 from pathlib import Path
 
 from orquestra.sdk._base import serde
-from orquestra.sdk._base._logs._interfaces import WorkflowLogs
+from orquestra.sdk._base._logs._interfaces import LogOutput, WorkflowLogs
 from orquestra.sdk.schema.workflow_run import TaskInvocationId, WorkflowRunId
 
 
@@ -78,17 +78,25 @@ class LogsDumper:
         dir_path: Path,
         wf_run_id: WorkflowRunId,
         log_type: t.Optional[WorkflowLogs.WorkflowLogTypeName] = None,
+        is_stderr: bool = False,
     ) -> Path:
         dir_path.mkdir(parents=True, exist_ok=True)
+        extension = "err" if is_stderr else "log"
         if log_type:
             return (
-                dir_path / f"{wf_run_id}_{log_type.value.lower().replace(' ', '_')}.log"
+                dir_path
+                / f"{wf_run_id}_{log_type.value.lower().replace(' ', '_')}.{extension}"
             )
-        return dir_path / f"{wf_run_id}.log"
+        return dir_path / f"{wf_run_id}.{extension}"
 
     def dump(
         self,
-        logs: t.Union[t.Mapping[TaskInvocationId, t.Sequence[str]], t.Sequence[str]],
+        logs: t.Union[
+            t.Mapping[TaskInvocationId, t.Sequence[str]],
+            t.Sequence[str],
+            LogOutput,
+            t.Mapping[TaskInvocationId, LogOutput],
+        ],
         wf_run_id: WorkflowRunId,
         dir_path: Path,
         log_type: t.Optional[WorkflowLogs.WorkflowLogTypeName] = None,
@@ -100,17 +108,23 @@ class LogsDumper:
         No standard errors are expected to be raised.
         """
         logs_file = self._get_logs_file(dir_path, wf_run_id, log_type=log_type)
+        err_logs_file = self._get_logs_file(
+            dir_path, wf_run_id, log_type=log_type, is_stderr=True
+        )
 
-        log_lines = self._construct_output_log_lines(logs)
+        out_lines, err_lines = self._construct_output_log_lines(logs)
 
         with logs_file.open("w") as f:
-            f.writelines(log_lines)
+            f.writelines("\n".join(out_lines))
 
-        return logs_file
+        with err_logs_file.open("w") as f:
+            f.writelines("\n".join(err_lines))
+
+        return logs_file, err_logs_file
 
     @singledispatchmethod
     @staticmethod
-    def _construct_output_log_lines(_, *args) -> t.List[str]:
+    def _construct_output_log_lines(*args) -> t.Tuple[t.List[str], t.List[str]]:
         """Construct a list of log lines to be printed.
 
         This method has overloads for dict and list arguments.
@@ -119,18 +133,34 @@ class LogsDumper:
             f"No log lines constructor for args {args}"
         )  # pragma: no cover
 
+    @_construct_output_log_lines.register(LogOutput)
+    @staticmethod
+    def _(logs: LogOutput) -> t.Tuple[t.List[str], t.List[str]]:
+        return logs.out, logs.err
+
     @_construct_output_log_lines.register(dict)
     @staticmethod
-    def _(logs: dict) -> t.List[str]:
+    def _(
+        logs: t.Union[
+            t.Mapping[TaskInvocationId, t.Sequence[str]],
+            t.Mapping[TaskInvocationId, LogOutput],
+        ]
+    ) -> t.Tuple[t.List[str], t.List[str]]:
         outlines = []
-        for task_invocation in logs:
-            outlines.append(f"Logs for task invocation: {task_invocation}:\n\n")
-            for log in logs[task_invocation]:
-                outlines.append(log + "\n")
-            outlines.append("\n\n")
-        return outlines
+        errlines = []
+        for task_invocation_id, log_values in logs.items():
+            outlines.append(f"stdout logs for task invocation: {task_invocation_id}:\n")
+            errlines.append(f"stderr logs for task invocation: {task_invocation_id}:\n")
+            if isinstance(log_values, LogOutput):
+                outlines.extend(log_values.out)
+                errlines.extend(log_values.err)
+            else:
+                outlines.extend(log_values)
+            outlines.append("\n")
+            errlines.append("\n")
+        return outlines, errlines
 
     @_construct_output_log_lines.register(list)
     @staticmethod
-    def _(logs: list) -> t.List[str]:
-        return [log + "\n" for log in logs]
+    def _(logs: list) -> t.Tuple[t.List[str], t.List[str]]:
+        return logs, []

zapata-engineering/orquestra-sdk

src/orquestra/sdk/_client/_base/cli/_dumpers.py
@@ -1,5 +1,5 @@
 ################################################################################
-# © Copyright 2023 Zapata Computing Inc.
+# © Copyright 2023-2024 Zapata Computing Inc.
 ################################################################################
 
 """Code that stores values on disk as a result of a CLI command."""
@@ -8,7 +8,7 @@ from functools import singledispatchmethod
 from pathlib import Path
 
 from orquestra.sdk._shared import serde
-from orquestra.sdk._shared._logs._interfaces import WorkflowLogs
+from orquestra.sdk._shared._logs._interfaces import LogOutput, WorkflowLogs
 from orquestra.sdk._shared.schema.workflow_run import TaskInvocationId, WorkflowRunId
 
 
@@ -78,17 +78,25 @@ class LogsDumper:
         dir_path: Path,
         wf_run_id: WorkflowRunId,
         log_type: t.Optional[WorkflowLogs.WorkflowLogTypeName] = None,
+        is_stderr: bool = False,
     ) -> Path:
         dir_path.mkdir(parents=True, exist_ok=True)
+        extension = "err" if is_stderr else "log"
         if log_type:
             return (
-                dir_path / f"{wf_run_id}_{log_type.value.lower().replace(' ', '_')}.log"
+                dir_path
+                / f"{wf_run_id}_{log_type.value.lower().replace(' ', '_')}.{extension}"
             )
-        return dir_path / f"{wf_run_id}.log"
+        return dir_path / f"{wf_run_id}.{extension}"
 
     def dump(
         self,
-        logs: t.Union[t.Mapping[TaskInvocationId, t.Sequence[str]], t.Sequence[str]],
+        logs: t.Union[
+            t.Mapping[TaskInvocationId, t.Sequence[str]],
+            t.Sequence[str],
+            LogOutput,
+            t.Mapping[TaskInvocationId, LogOutput],
+        ],
         wf_run_id: WorkflowRunId,
         dir_path: Path,
         log_type: t.Optional[WorkflowLogs.WorkflowLogTypeName] = None,
@@ -100,17 +108,23 @@ class LogsDumper:
         No standard errors are expected to be raised.
         """
         logs_file = self._get_logs_file(dir_path, wf_run_id, log_type=log_type)
+        err_logs_file = self._get_logs_file(
+            dir_path, wf_run_id, log_type=log_type, is_stderr=True
+        )
 
-        log_lines = self._construct_output_log_lines(logs)
+        out_lines, err_lines = self._construct_output_log_lines(logs)
 
         with logs_file.open("w") as f:
-            f.writelines(log_lines)
+            f.writelines("\n".join(out_lines))
 
-        return logs_file
+        with err_logs_file.open("w") as f:
+            f.writelines("\n".join(err_lines))
+
+        return logs_file, err_logs_file
 
     @singledispatchmethod
     @staticmethod
-    def _construct_output_log_lines(_, *args) -> t.List[str]:
+    def _construct_output_log_lines(*args) -> t.Tuple[t.List[str], t.List[str]]:
         """Construct a list of log lines to be printed.
 
         This method has overloads for dict and list arguments.
@@ -119,18 +133,34 @@ class LogsDumper:
             f"No log lines constructor for args {args}"
         )  # pragma: no cover
 
+    @_construct_output_log_lines.register(LogOutput)
+    @staticmethod
+    def _(logs: LogOutput) -> t.Tuple[t.List[str], t.List[str]]:
+        return logs.out, logs.err
+
     @_construct_output_log_lines.register(dict)
     @staticmethod
-    def _(logs: dict) -> t.List[str]:
+    def _(
+        logs: t.Union[
+            t.Mapping[TaskInvocationId, t.Sequence[str]],
+            t.Mapping[TaskInvocationId, LogOutput],
+        ]
+    ) -> t.Tuple[t.List[str], t.List[str]]:
         outlines = []
-        for task_invocation in logs:
-            outlines.append(f"Logs for task invocation: {task_invocation}:\n\n")
-            for log in logs[task_invocation]:
-                outlines.append(log + "\n")
-            outlines.append("\n\n")
-        return outlines
+        errlines = []
+        for task_invocation_id, log_values in logs.items():
+            outlines.append(f"stdout logs for task invocation: {task_invocation_id}:\n")
+            errlines.append(f"stderr logs for task invocation: {task_invocation_id}:\n")
+            if isinstance(log_values, LogOutput):
+                outlines.extend(log_values.out)
+                errlines.extend(log_values.err)
+            else:
+                outlines.extend(log_values)
+            outlines.append("\n")
+            errlines.append("\n")
+        return outlines, errlines
 
     @_construct_output_log_lines.register(list)
     @staticmethod
-    def _(logs: list) -> t.List[str]:
-        return [log + "\n" for log in logs]
+    def _(logs: list) -> t.Tuple[t.List[str], t.List[str]]:
+        return logs, []