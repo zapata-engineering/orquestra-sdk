c6475b420d54544d4cd7daba3be75539b14aec94
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2024-05-21 14:20:03 +0200

    add tests for CE methods

--- projects/orquestra-sdk/tests/sdk/test_consistent_return_shapes.py
+++ projects/orquestra-sdk/tests/sdk/test_consistent_return_shapes.py
@@ -121,7 +121,7 @@ def mock_config_env_var(
             '    "config_name": "CE",\n'
             '    "runtime_name": "CE_REMOTE",\n'
             '    "runtime_options": {\n'
-            '      "token": "nice",\n'
+            '      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",\n'  # noqa
             f'      "uri": "http://127.0.0.1:{httpserver.port}"\n'
             "  }\n"
             "  }\n"
