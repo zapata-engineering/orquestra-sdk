2fbc2db569106857aed4b26401a31bb85d98a179
Sebastian Morawiec <Sebastian.Morawiec@zapatacomputing.com>
2024-06-27 14:58:56 +0200

    review adjustment - reorder

--- projects/orquestra-sdk/src/orquestra/sdk/_client/_base/_config/__init__.py
+++ projects/orquestra-sdk/src/orquestra/sdk/_client/_base/_config/__init__.py
@@ -2,7 +2,7 @@ from ._api import RuntimeConfig, get_config_option, migrate_config_file, resolve
 
 __all__ = [
     "RuntimeConfig",
+    "get_config_option",
     "migrate_config_file",
     "resolve_config",
-    "get_config_option",
 ]
